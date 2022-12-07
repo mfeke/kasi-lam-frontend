10e947d6b1a0463d25c3a93b4edad72efef485f6
Thulane Mfeketho <mfekethothulane@gmail.comm>
2022-12-07 14:01:51 +0200

    update

--- src/app/components/form-checkout/form-checkout.component.ts
+++ src/app/components/form-checkout/form-checkout.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-checkout',
@@ -9,7 +11,9 @@ export class FormCheckoutComponent implements OnInit {
 
   constructor() { }
 
+
+
   ngOnInit(): void {
   }
-
+   
 }
